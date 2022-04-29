a190ab55826affd732f2d5ffe10f786ca2387c89
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-04-29 09:33:17 -0300

    Ciclos de Pagamentos App - Componente cabeçalho

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -12,14 +12,14 @@ module.exports = {
         contentBase: './public',
     },
     resolve: {
-        extensions: ['','.js','.jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins : [
+    plugins: [ 
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
@@ -36,10 +36,10 @@ module.exports = {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
-        },{
+        }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        },{
+        }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
         }]
