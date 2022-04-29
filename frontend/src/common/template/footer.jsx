da40a57833f8180b796d58117574948982cf7504
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-04-29 13:07:19 -0300

    Ciclos de Pagamentos App - Componente Rodapé

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2022 
+            <a href="https://thiagohbfialho.github.io/" target='_blank'> Thiago Fialho</a>
+        </strong>
+    </footer>
+)
