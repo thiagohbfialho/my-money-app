e1e86f0b3021642d6ec088f90c34bdc67e5f44ec
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-04-29 16:35:42 -0300

    Ciclos de Pagamentos App - Configurando o redux e integrando com o dashboaboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+import { getSummay } from "./dashboardActions";
 import Row from "../common/layout/row";
 import Content from "../common/template/content";
 import ContentHeader from "../common/template/contentHeader";
 import ValueBox from "../common/widget/valueBox";
 
 class Dashboard extends Component {
+
+    componentWillMount(){
+        this.props.getSummay()
+    }
+
     render(){
+        const {credit, debt} = this.props.summary
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank'
-                            value='R$ 10' text='Total de Créditos' />
+                            value={`R$ ${credit}`} text='Total de Créditos' />
                         <ValueBox cols='12 4' color='red' icon='credit card'
-                            value='R$ 10' text='Total de Débitos' />
+                            value={`R$ ${debt }`} text='Total de Débitos' />
                         <ValueBox cols='12 4' color='blue' icon='money'
-                            value='R$ 0' text='Valor Consolidado' />
+                            value={`R$ ${credit - debt}`} text='Valor Consolidado' />
                     </Row>
                 </Content>
             </div>
@@ -24,4 +34,6 @@ class Dashboard extends Component {
     }
 }
 
-export default Dashboard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+const mapDispatchToProps = dispatch => bindActionCreators({getSummay}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
