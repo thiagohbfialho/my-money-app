35f2cc087e22315aa844ae928061a63080ab5d88
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-04-30 10:45:58 -0300

    Ciclos de Pagamentos App - Criando o TabReducer e TabAction

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import tabReducer from "../common/tab/tabReducer";
 import DashboardReducer from "../dashboard/dashboardReducer";
 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer
+    dashboard: DashboardReducer,
+    tab: tabReducer
 })
 
 export default rootReducer
