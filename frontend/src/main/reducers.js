9eedfedecd8d31873c6a26ae8a88dea9ef43d78c
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-02 09:24:39 -0300

    Ciclos de pagamentos - Criando e integrando o componente BillingCycleList com redux

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
-import tabReducer from "../common/tab/tabReducer";
+import TabReducer from "../common/tab/tabReducer";
 import DashboardReducer from "../dashboard/dashboardReducer";
+import BillingCycleReducer from "../billingCycle/billingCycleReducer";
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: tabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer 
 })
 
 export default rootReducer
