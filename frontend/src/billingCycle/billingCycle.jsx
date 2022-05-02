02cabea51c788b99fc13c28c83d39ef56524d798
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-02 10:21:30 -0300

    Ciclos de pagamentos - CriBillingCycleForm com redux-form

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 
-import { selectTab, showTabs } from "../common/tab/tabActions";
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from "../common/tab/tabs";
@@ -10,7 +9,11 @@ import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
 import TabContent from "../common/tab/tabContent";
+import { selectTab, showTabs } from "../common/tab/tabActions";
+import { create } from "./billingCycleActions";
+
 import List from "./billingCycleList";
+import Form from "./billingCycleForm"
 
 class BillingCycle extends Component {
 
@@ -35,7 +38,9 @@ class BillingCycle extends Component {
                             <TabContent id='tabList'>
                                 <List/>
                             </TabContent>
-                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create}/>
+                            </TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
@@ -46,5 +51,7 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+    selectTab, showTabs, create
+}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
