fb6d04b52bb4075f8f688e6b441f407bc50b1f2e
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-02 16:11:49 -0300

    Ciclos de pagamentos - Exibir aba de alterar ciclos de pagamentos e inicializar formulário com dados

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
-import { getList } from './billingCycleActions'
+import { getList, showUpdate } from './billingCycleActions'
 
 class BillingCylceList extends Component {
 
@@ -16,6 +16,11 @@ class BillingCylceList extends Component {
                 <td>{bc.name}</td>
                 <td>{bc.month}</td>
                 <td>{bc.year}</td>
+                <td>
+                    <button className="btn btn-warning" onClick={() => this.props.showUpdate(bc)}>
+                        <i className="fa fa-pencil"></i>
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -29,6 +34,7 @@ class BillingCylceList extends Component {
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
+                            <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -41,5 +47,5 @@ class BillingCylceList extends Component {
 }
 
 const mapStateToProps = state => ({list: state.billingCycle.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCylceList)
