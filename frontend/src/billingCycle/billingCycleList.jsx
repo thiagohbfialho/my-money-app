299d9288157e0c8fb0d593760fd6644e4c46684a
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-03 20:56:43 -0300

    Ciclos de Pagamentos - Finalizando o cadastro básico e melhorias

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -37,7 +37,7 @@ class BillingCylceList extends Component {
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
-                            <th>Ações</th>
+                            <th className="table-actions">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
