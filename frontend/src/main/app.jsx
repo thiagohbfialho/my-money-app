95346f56342f3688461fa47e2eb30507c7c27dba
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-08 23:19:03 -0300

    Ciclos de pagamentos - autenticação frontend

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,5 @@
-import '../common/template/dependencies'
 import React from "react";
+
 import Header from '../common/template/header';
 import SideBar from '../common/template/sideBar';
 import Footer from '../common/template/footer';
