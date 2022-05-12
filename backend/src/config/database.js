cfcd78382303ce9cf1563d128944628f52e59fc6
Thiago Fialho <thiago.fialhoti@gmail.com>
2022-05-12 13:46:45 -0300

    Ciclos de pagamentos - fazendo deploy do backend no heroku

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney', {
-    useNewUrlParser: true })
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb//localhost/mymoney'
+module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,4 +1,4 @@
-const port = 3003
+const port = process.env.PORT
 
 const bodyParser = require('body-parser')
 const express = require('express')
